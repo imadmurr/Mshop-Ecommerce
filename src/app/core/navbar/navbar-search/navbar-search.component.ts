c64f36d822dea3a334983558c5cd96d18d2dc59f
Imad <imad@Imads-MacBook.local>
2025-03-02 00:34:24 +0200

    Setup initial project

---
+++ src/app/core/navbar/navbar-search/navbar-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-search',
+  imports: [],
+  templateUrl: './navbar-search.component.html',
+  styleUrl: './navbar-search.component.scss'
+})
+export class NavbarSearchComponent {
+
+}
---
+++ src/app/core/navbar/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar1',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
