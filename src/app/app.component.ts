8a4827426e4adb40d731c88bb05bd06467bee7b5
Imad <imad@Imads-MacBook.local>
2025-03-02 00:34:24 +0200

    Setup initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {FooterComponent} from './core/footer/footer.component';
+import {NavbarComponent} from './core/navbar/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
