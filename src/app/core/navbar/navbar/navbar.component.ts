aa93e3430ec28d1f6b53fee5883fde4ec3456ffa
Imad <imad@Imads-MacBook.local>
2025-03-09 16:12:08 +0200

    Add navbar menu and user/cart icons

--- src/app/core/navbar/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar/navbar.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import {NgIf} from '@angular/common';
+import {NgIf, NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-navbar1',
   imports: [
-    NgIf
+    NgIf,
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isAuthenticated = true; // Change this dynamically based on authentication state
 
+  signIn() {
+    // Simulate sign-in (replace with actual auth logic)
+    this.isAuthenticated = true;
+  }
 }
